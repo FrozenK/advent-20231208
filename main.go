0e33070a4012cfadff994b72bbe546361a377204
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2024-02-17 10:17:20 +0100

    add part 2

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ import (
 )
 
 type node struct {
-	name  string
-	left  string
-	right string
+	name    string
+	left    string
+	right   string
+	starter bool
+	ender   bool
 }
 
 func main() {
-	f, err := os.Open("input2.txt")
+	f, err := os.Open("input3.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -23,6 +25,7 @@ func main() {
 	nodes := make(map[string]node)
 
 	var actions []string
+	var positions []string
 	ln := 1
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -30,39 +33,56 @@ func main() {
 		if ln == 1 {
 			actions = strings.Split(line, "")
 		} else if line != "" {
+			starter := false
+			ender := false
 			inputs := strings.Split(line, "=")
 			directions := strings.Split(strings.Replace(strings.Replace(inputs[1], "(", "", 1), ")", "", 1), ",")
-			nodes[strings.TrimSpace(inputs[0])] = node{
-				name:  strings.TrimSpace(inputs[0]),
-				left:  strings.TrimSpace(directions[0]),
-				right: strings.TrimSpace(directions[1]),
+
+			name := strings.TrimSpace(inputs[0])
+			data := strings.Split(name, "")
+			if data[2] == "A" {
+				positions = append(positions, name)
+				starter = true
+			}
+			if data[2] == "Z" {
+				ender = true
+			}
+			nodes[name] = node{
+				name:    name,
+				left:    strings.TrimSpace(directions[0]),
+				right:   strings.TrimSpace(directions[1]),
+				starter: starter,
+				ender:   ender,
 			}
 		}
 		ln++
 	}
 
 	sum := 0
-	start := "AAA"
-	end := "ZZZ"
-	position := start
-
-	found := false
+	toFound := len(positions)
+	found := 0
 	for true {
-		for _, a := range actions {
-			n := nodes[position]
-			if n.name == end {
-				found = true
-				break
-			}
-			switch a {
-			case "R":
-				position = n.right
-			case "L":
-				position = n.left
+		for i, p := range positions {
+			for _, a := range actions {
+				fmt.Println("---------")
+				fmt.Println("Action = ", a)
+				found := 0
+				n := nodes[p]
+				if n.ender {
+					found++
+					break
+				}
+				switch a {
+				case "R":
+					positions[i] = n.right
+				case "L":
+					positions[i] = n.left
+				}
+				fmt.Println(positions[i])
 			}
 			sum++
 		}
-		if found {
+		if found == toFound {
 			break
 		}
 	}
